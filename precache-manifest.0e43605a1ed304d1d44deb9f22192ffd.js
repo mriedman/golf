88d92317b1ee93a6f6902ab593d0ecdd9f536576
mriedman <33470702+mriedman@users.noreply.github.com>
2019-08-21 21:20:53 -0400

    Add files via upload

---
+++ precache-manifest.0e43605a1ed304d1d44deb9f22192ffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee1f3617c1006ffb7cc2a5611455876",
+    "url": "mriedman.github.io/golf/index.html"
+  },
+  {
+    "revision": "3fd161625ea5ba2e714d",
+    "url": "mriedman.github.io/golf/static/css/main.b640b675.chunk.css"
+  },
+  {
+    "revision": "7cab911dd33f5f41d3fe",
+    "url": "mriedman.github.io/golf/static/js/2.6c5bef6a.chunk.js"
+  },
+  {
+    "revision": "3fd161625ea5ba2e714d",
+    "url": "mriedman.github.io/golf/static/js/main.d65eb666.chunk.js"
+  },
+  {
+    "revision": "181cc29713221ac38f4b",
+    "url": "mriedman.github.io/golf/static/js/runtime~main.90b23e35.js"
+  }
+]);
