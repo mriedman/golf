c49ab2d4c5a4ee97aaf1e43b7370fbcab92d415b
Matthew Riedman <mriedman@Matthews-MacBook-Air.local>
2020-05-21 14:39:06 -0400

    last try?

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "mriedman.github.io/golf/precache-manifest.0e43605a1ed304d1d44deb9f22192ffd.js"
+  "/mriedman.github.io/golf/precache-manifest.f51bc68ee34af18aaabc64000092bf3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("mriedman.github.io/golf/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mriedman.github.io/golf/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
